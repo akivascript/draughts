36fdaac7ed606e8f31eb87ca6b5d435645fbbefd
Akiva <git@misanthropicgeek.com>
2014-09-01 11:53:13 -0500

    Cleans up unnecessary object copying.

--- src/draughts.js
+++ src/draughts.js
@@ -13,17 +13,16 @@
 	}
 
 	function setupBoard (board) {
-		var b = board;
 		var setupPieces = function (row) {
-		var col = 0;
+			var col = 0;
 
 			if (isEven (row)) {
 				for (col = 1; col < 8; col = col + 2) {
-					if (isOdd (col)) { placePiece (b, player, row, col); }
+					if (isOdd (col)) { placePiece (board, player, row, col); }
 				}
 			} else {
 				for (col = 0; col < 8; col = col + 2) {
-					if (isEven (col)) { placePiece (b, player, row, col); }
+					if (isEven (col)) { placePiece (board, player, row, col); }
 				}
 			}
 		};
@@ -31,33 +30,30 @@
 		var player = 1;
 		_.each (player1rows, setupPieces);
 
-		player =2;
+		player = 2;
 		_.each (player2rows, setupPieces);
 
-		return b;
+		return board;
 	}
 
 	function placePiece (board, player, row, col) {
-		var b = board;
 		var size = board.length;
 
-		if (!isLegalSpace (b, row, col)) {
+		if (!isLegalSpace (board, row, col)) {
 			throw new Error ('The destination space [' + row + '][' + col + '] ' +
 											 'is beyond the boundaries of the board size of ' +
 											 size + 'x' + size + '.');
 		}
 
-		b [row][col] = player;
+		board [row][col] = player;
 
-		return b;
+		return board;
 	}
 
 	function removePiece (board, row, col) {
-		var b = board;
-
-		b [row][col] = 0;
+		board [row][col] = 0;
 		
-		return b;
+		return board;
 	}
 
 	function examineSpace (board, row, col) {
@@ -77,7 +73,6 @@
 
 	// ---- Player Functions ---------------------------------------------------------
 	function movePiece (board, player, row, col) {
-		var b = board;
 		var dir;
 
 		if (player === player1) { dir = -1; } else { dir = 1; }
@@ -90,7 +85,7 @@
 											 row + '][' + col + ']');
 		}
 
-		if (!isLegalSpace (b, destRow, col)) {
+		if (!isLegalSpace (board, destRow, col)) {
 			throw new Error ('Player ' + player + ' attempted to move a piece off ' +
 											 'the board to [' + destRow + '][' + col + ']');
 		}
@@ -102,10 +97,10 @@
 											 '] is already occupied.');
 		}
 
-		b = removePiece (b, row, col);
-		b = placePiece (b, player, destRow, col);
+		board = removePiece (board, row, col);
+		board = placePiece (board, player, destRow, col);
 		
-		return b;
+		return board;
 	}
 
 	function jumpPiece (board, player, row, col, dir) {
@@ -137,10 +132,8 @@
 			}
 		}
 
-		var b = board;
-
-		if (!isLegalSpace (b, destRow, destCol)) {
-			var size = b.length;
+		if (!isLegalSpace (board, destRow, destCol)) {
+			var size = board.length;
 			
 			throw new Error ('The destination space [' + destRow + '][' + destCol +
 											 '] is beyond the boundaries of the board size of ' +
@@ -157,11 +150,11 @@
 											 jumpRow + '][' + jumpCol + ']');
 		}
 
-		b = removePiece (board, row, col);
-		b = removePiece (board, jumpRow, jumpCol);
-		b = placePiece (b, player, destRow, destCol);
+		board = removePiece (board, row, col);
+		board = removePiece (board, jumpRow, jumpCol);
+		board = placePiece (board, player, destRow, destCol);
 
-		return b;
+		return board;
 	}
 
 	// ---- Utility Functions --------------------------------------------------------
